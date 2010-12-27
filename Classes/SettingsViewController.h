e0dd14b218989edf3e7cffd837a61719fb7c8686
ecordell <cordell.evan@gmail.com>
2010-12-27 10:54:22 -0600

    Adding settings files

---
+++ Classes/SettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  Trees
+//
+//  Created by Evan on 12/27/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsViewController : UIViewController {
+
+}
+
+@end
