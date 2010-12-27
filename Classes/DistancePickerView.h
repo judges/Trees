0d5486a8c0bdfcf20b46117ebd5fe8c7daa0e03c
ecordell <cordell.evan@gmail.com>
2010-12-27 15:50:48 -0600

    Added custom DistancePicker (not implemented yet)

---
+++ Classes/DistancePickerView.h
@@ -0,0 +1,16 @@
+//
+//  DistancePickerView.h
+//  Trees
+//
+//  Created by Evan on 12/27/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DistancePickerView : UIView {
+
+}
+
+@end
---
+++ Classes/DistancePickerView.m
@@ -0,0 +1,37 @@
+//
+//  DistancePickerView.m
+//  Trees
+//
+//  Created by Evan on 12/27/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "DistancePickerView.h"
+
+
+@implementation DistancePickerView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code.
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code.
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
