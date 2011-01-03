188aa98e61d24e6b79ca5a0f9c4c98543295da1c
bigsean <sclifford@gmail.com>
2011-01-03 16:05:03 -0600

    TreeDetailHeader additions - just structure

---
+++ Classes/TreeDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  TreeDetailViewController.h
+//  Trees
+//
+//  Created by Sean Clifford on 1/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TreeDetailViewController : UIViewController {
+
+}
+
+@end
